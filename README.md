dee8d545a74fd2fd65196b2941cdbe31f37a159d
Aaron Lichtman <aaronlichtman@gmail.com>
2023-01-27 03:58:30 -0800

    Port issues to github

--- README.md
+++ README.md
@@ -1,18 +1,38 @@
 # rofi-insect
 
-[insect](https://github.com/sharkdp/insect) for [`rofi`](https://github.com/davatorium/rofi) / `dmenu`
+[insect](https://github.com/sharkdp/insect) plugin for [`rofi`](https://github.com/davatorium/rofi) / `dmenu`
 
-![Screencast](./demo.gif)
+https://user-images.githubusercontent.com/20600565/215080362-7c188e15-2d56-421d-abec-9bf88b44df60.mp4
 
-## Installation
-
-I integrate this with [`rofi-bangs.sh`](https://github.com/gotbletu/shownotes/blob/master/rofi-scripts-collection/rofi-bangs.sh).
-
-! TODO: Add detailed instructions
+> **Note**
+> Watch the demo in full screen for much better quality.
 
 ## Usage
 
 `insect` is used as the calculator back-end. You can pass any operation `insect` can handle.
+
+## Installation + Configuration
+
+I integrate this with [`rofi-bangs.sh`](https://github.com/gotbletu/shownotes/blob/master/rofi-scripts-collection/rofi-bangs.sh).
+
+```bash
+## ~/.config/rofi/scripts/rofi-bangs.sh
+...
+# Use calculator (insect)
+COMMANDS["calc"]="/home/alichtman/.config/rofi/scripts/rofi-insect/rofi-insect.sh"
+LABELS["calc"]=""
+```
+
+And I have `rofi-bangs` integrated with [`sxhkd`](https://github.com/baskerville/sxhkd).
+
+```
+## ~/.config/sxhkd/sxhkdrc
+...
+alt + @m
+        ~/.config/rofi/scripts/rofi-bangs.sh
+```
+
+I press `Alt + m` on my keyboard, and get the `rofi-bangs` popup. I can then enter `calc` mode.
 
 ## Dependencies
 
--- rofi-insect.sh
+++ rofi-insect.sh
@@ -2,23 +2,18 @@
 
 # Rofi integration for insect (https://github.com/sharkdp/insect)
 # Copying to the clipboard requires xsel
+# Modified from: https://github.com/onespaceman/menu-calc
 
-usage() {
-    echo "$(tput bold)rofi-insect calculator$(tput sgr0)
+function usage() {
+    echo "$(tput bold)rofi-insect calculator plugin$(tput sgr0)
 An insect (fancy calculator that does unit conversions) plugin for Rofi or dmenu
 
 This script supports (almost) anything insect does.
 The previous result is ALWAYS passed as the first argument
 
-$(tput bold)############
-# KNOWN BUGS:
-############$(tput sgr0)
+$(tput bold)Clear if results start not making sense!$(tput sgr0)
 
-0. Hit Clear if results start not making sense.
-1. Crashes if \`-NUMBER\` is entered after an expression has been calculated.
-     Reproduce with: 500 + 50 [ENTER] -4
-2. 500 [ENTER] 500 + 400 [ENTER] results in the wrong answer. For some reason, the first 500 isn't being cleared properly. idk.
-"
+Report bugs at: https://github.com/alichtman/rofi-insect/issues/new"
     exit
 }
 
@@ -31,7 +26,6 @@ case "$1" in
     -h|-help|--help|help) usage ;;
 esac
 
-# Path to menu application
 if [[ -n $(command -v rofi) ]]; then
     menu="$(command -v rofi) -dmenu"
 elif [[ -n $(command -v dmenu) ]]; then
@@ -43,7 +37,7 @@ fi
 
 input="$*"
 answer=$(echo "$input" | insect)
-action=$(echo -e "Copy to clipboard\nClear\nClose" | $menu -i -p "$input = $answer") # Check out $ man dmenu for an explanation these options
+action=$(echo -e "Copy to clipboard\nClear\nClose" | $menu -i -p "$input = $answer") # See $ man dmenu for an explanation these options
 
 case $action in
     "Clear") $0 ;;
